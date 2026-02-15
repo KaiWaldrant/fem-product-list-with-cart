166f37a4fb1fd8229f665993081aa35b296aa776
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-15 23:05:33 +0100

    Add data json types

---
+++ src/types/json.ts
@@ -0,0 +1,13 @@
+export type Image = {
+  thumbnail: string;
+  mobile: string;
+  tablet: string;
+  desktop: string;
+};
+
+export type Product = {
+  image: Image;
+  name: string;
+  category: string;
+  price: number;
+};
