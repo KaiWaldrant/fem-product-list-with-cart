84248f61606e0d1549126f02d5895945ab4548e2
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-17 22:55:23 +0100

    run biome and fix errors

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -1,34 +1,42 @@
-import  IconAddToCart from "../assets/icons/icon-add-to-cart"
-import IconDecrementQuantity from "../assets/icons/icon-decrement-quantity";
-import IconIncrementQuantity from "../assets/icons/icon-increment-quantity";
+import IconAddToCart from "../assets/icons/icon-add-to-cart"
+import IconDecrementQuantity from "../assets/icons/icon-decrement-quantity"
+import IconIncrementQuantity from "../assets/icons/icon-increment-quantity"
 
 export interface CartButtonProps {
-  isInCart: boolean;
-  quantity: number;
-  handleDecrease: () => void;
-  handleIncrease: () => void;
+	isInCart: boolean
+	quantity: number
+	handleDecrease: () => void
+	handleIncrease: () => void
 }
 
-export function CartButton({isInCart, quantity, handleDecrease, handleIncrease} : CartButtonProps) {
-  return (
-    <>
-      { isInCart? (
-        <button className="btn btn-update" >
-          <a onClick={() => handleDecrease()}>
-            <IconDecrementQuantity />
-          </a>
-          <p>{quantity}</p>
-          <a onClick={() => handleIncrease()}>
-            <IconIncrementQuantity />
-          </a>
-        </button>
-      ) : (
-        <button className="btn btn-add" onClick={() => handleIncrease()}>
-          <IconAddToCart />
-          <p>Add to Cart</p>
-        </button>
-      )}
-      
-    </>
-  );
+export function CartButton({
+	isInCart,
+	quantity,
+	handleDecrease,
+	handleIncrease,
+}: CartButtonProps) {
+	return (
+		<>
+			{isInCart ? (
+				<button type="button" className="btn btn-update">
+					<button type="button" onClick={() => handleDecrease()}>
+						<IconDecrementQuantity />
+					</button>
+					<p>{quantity}</p>
+					<button type="button" onClick={() => handleIncrease()}>
+						<IconIncrementQuantity />
+					</button>
+				</button>
+			) : (
+				<button
+					type="button"
+					className="btn btn-add"
+					onClick={() => handleIncrease()}
+				>
+					<IconAddToCart />
+					<p>Add to Cart</p>
+				</button>
+			)}
+		</>
+	)
 }
--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -1,37 +1,47 @@
-import type { Product } from "../types/json";
-import type { CartType } from "../types/cart";
-import {CartButton} from "./cart-button";
+import type { CartType } from "../types/cart"
+import type { Product } from "../types/json"
+import { CartButton } from "./cart-button"
 
 export interface ItemCardProps {
-  product: Product;
-  cart: CartType;
-  updateCartQuantity: (productName: string, change: number) => void;
+	product: Product
+	cart: CartType
+	updateCartQuantity: (productName: string, change: number) => void
 }
 
-export default function ItemCard({ product , cart, updateCartQuantity } : ItemCardProps) {
+export default function ItemCard({
+	product,
+	cart,
+	updateCartQuantity,
+}: ItemCardProps) {
+	const quantity = cart[product.name]?.quantity || 0
+	const isInCart = quantity > 0
 
-  const quantity = cart[product.name]?.quantity || 0;
-  const isInCart = quantity > 0;
+	const handleIncrease = () => updateCartQuantity(product.name, 1)
+	const handleDecrease = () => updateCartQuantity(product.name, -1)
 
-  const handleIncrease = () => updateCartQuantity(product.name, 1)
-  const handleDecrease = () => updateCartQuantity(product.name, -1)
-
-  return (
-    <div className="item-card">
-      <div className={` item-card__header ${isInCart ? "item-card__header--in-cart" : ""}`}>
-        <picture>
-          <source srcSet={product.image.mobile} media="(max-width: 767px)" />
-          <source srcSet={product.image.tablet} media="(max-width: 1439px)" />
-          <source srcSet={product.image.desktop} media="(min-width: 1440px)" />
-          <img src={product.image.thumbnail} alt={product.name} />
-        </picture>
-        <CartButton isInCart={isInCart} quantity={quantity}  handleDecrease={handleDecrease} handleIncrease={handleIncrease} />
-      </div>
-      <div className="item-card__info">
-        <p className="product-category">{product.category}</p>
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-price">${product.price.toFixed(2)}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="item-card">
+			<div
+				className={` item-card__header ${isInCart ? "item-card__header--in-cart" : ""}`}
+			>
+				<picture>
+					<source srcSet={product.image.mobile} media="(max-width: 767px)" />
+					<source srcSet={product.image.tablet} media="(max-width: 1439px)" />
+					<source srcSet={product.image.desktop} media="(min-width: 1440px)" />
+					<img src={product.image.thumbnail} alt={product.name} />
+				</picture>
+				<CartButton
+					isInCart={isInCart}
+					quantity={quantity}
+					handleDecrease={handleDecrease}
+					handleIncrease={handleIncrease}
+				/>
+			</div>
+			<div className="item-card__info">
+				<p className="product-category">{product.category}</p>
+				<h3 className="product-name">{product.name}</h3>
+				<p className="product-price">${product.price.toFixed(2)}</p>
+			</div>
+		</div>
+	)
 }
