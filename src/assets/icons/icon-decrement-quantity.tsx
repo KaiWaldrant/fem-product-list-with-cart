b3d614fdd260ab2cf461047d2bbadc1dd01e7744
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-17 19:53:22 +0100

    fix icons

--- src/assets/icons/icon-decrement-quantity.tsx
+++ src/assets/icons/icon-decrement-quantity.tsx
@@ -1,5 +1,14 @@
 export default function IconDecrementQuantity() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
-  );
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="10"
+			height="10"
+			fill="none"
+			viewBox="0 0 10 2"
+		>
+			<title>Decrement quantity</title>
+			<path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+		</svg>
+	)
 }
